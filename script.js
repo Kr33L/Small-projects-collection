739cd7235c73a7dfbfcaa211459fcca98b2c14d6
Kr33L <105356599+Kr33L@users.noreply.github.com>
2022-07-02 22:27:19 +0100

    changed some logic

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 addEventListener("submit", commentLimit);
 addEventListener("input", commentLength);
 
+
+
 function commentLimit(event) {
     let str = document.getElementById("comment").value;
     let style = document.getElementById("comment").style;
     let userInput = document.getElementById("user-comment");
     
+    userInput.innerHTML = "Your comment: " + str;
+    event.preventDefault();
+    
     if (str.length > 140) {
-        event.preventDefault();
         style.borderColor = "red";
-    }
-    else (str.length <= 140); {
-        event.preventDefault();
-        userInput.innerHTML = "Your comment: " + str;
     }
 }
 
