9bcd6a2a74d3636711dfd01bf9a0f11fb2aa7dea
Kr33L <105356599+Kr33L@users.noreply.github.com>
2022-07-02 20:10:12 +0100

    basic functionality complete

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 addEventListener("submit", commentLimit);
-document.getElementById("comment").addEventListener("input", commentLength)
+addEventListener("input", commentLength);
+
 function commentLimit(event) {
-        let text;
-        let style;
-        text = document.getElementById("comment").value;
-        style = document.getElementById("comment").style;
-        if (text.length > 140) {
-            event.preventDefault();
-            style.borderColor = "red";
-        }
+    let str = document.getElementById("comment").value;
+    let style = document.getElementById("comment").style;
+    let userInput = document.getElementById("user-comment");
+    
+    if (str.length > 140) {
+        event.preventDefault();
+        style.borderColor = "red";
+    }
+    else (str.length <= 140); {
+        event.preventDefault();
+        userInput.innerHTML = "Your comment: " + str;
+    }
 }
 
 function commentLength() {
-
+    let str = document.getElementById("comment").value;
+    let charVal = document.getElementById("char-value");
+    charVal.innerHTML = str.length + "/140";
 }
