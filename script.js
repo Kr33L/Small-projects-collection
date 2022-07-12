56c62e1aca4771fb949243b10c8933d5517a19a0
Kr33L <105356599+Kr33L@users.noreply.github.com>
2022-07-13 00:46:17 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const add = document.querySelectorAll(".add");
+const price = document.querySelectorAll(".price");
+
+add[0].onclick = () => {
+  //increase value of total by 
+}
+
+/* const basket = {
+  apple: {
+    name: "Apple",
+    img: "",
+    price: 0.5,
+  },
+  orange: {
+    name: "Orange",
+    img: "",
+    price: 0.5,
+  },
+  banana: {
+    name: "",
+    img: "",
+    price: 0.5,
+  },
+  strawberries: {
+    name: "",
+    img: "",
+    price: 0.5,
+  },
+  raspberries: {
+    name: "",
+    img: "",
+    price: 0.5,
+  },
+  blueberries: {
+    name: "",
+    img: "",
+    price: 0.5,
+  },
+};
+
+console.log(typeof basket);
+ */
