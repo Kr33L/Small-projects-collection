21e75020d6151c606f2458be04ad798c6abcc9dd
Kr33L <105356599+Kr33L@users.noreply.github.com>
2022-07-15 11:59:41 +0100

    cleanup and comments

--- script.js
+++ script.js
@@ -1,38 +1,28 @@
-var total = 0;
+var total = 0; //initialize total
 
 function addToTotal(event) {
-    const mapPricesId = `price-${event.target.id.slice(-1)}`;
-    const prices = document.getElementById(mapPricesId);
-    const totalInPage = document.getElementById('total');
-    total += parseFloat(prices.innerHTML.slice(1));
-    totalInPage.innerHTML = "Your total is:" + "£" + total.toFixed(2);
-}
-
-function removeFromTotal(event) {
-  const mapPricesId = `price-${event.target.id.slice(-1)}`;
-  const prices = document.getElementById(mapPricesId);
-  const totalInPage = document.getElementById('total');
-  total -= parseFloat(prices.innerHTML.slice(1));
-
+  const mapPricesId = `price-${event.target.id.slice(-1)}`; // map all prices
+  const prices = document.getElementById(mapPricesId); // get all mapped prices
+  const totalInPage = document.getElementById("total");
+  total += parseFloat(prices.innerHTML.slice(1)); //take off £ from html for conversion
   totalInPage.innerHTML = "Your total is:" + "£" + total.toFixed(2);
 }
-
-const removeBtns = document.querySelectorAll(".btn-remove");
-for (let i = 0; i < removeBtns.length; i++) {
-  removeBtns[i].addEventListener("click", removeFromTotal);
-}
-
 const addBtns = document.querySelectorAll(".btn-add");
 for (let i = 0; i < addBtns.length; i++) {
   addBtns[i].addEventListener("click", addToTotal);
 }
 
-
-
-/* function Basket() {
-  let sum = 0;
-  for (let i = 0; i )
-}; */
+function removeFromTotal(event) { //inverse of addToTotal
+  const mapPricesId = `price-${event.target.id.slice(-1)}`;
+  const prices = document.getElementById(mapPricesId);
+  const totalInPage = document.getElementById("total");
+  total -= parseFloat(prices.innerHTML.slice(1));
+  totalInPage.innerHTML = "Your total is:" + "£" + total.toFixed(2);
+}
+const removeBtns = document.querySelectorAll(".btn-remove");
+for (let i = 0; i < removeBtns.length; i++) {
+  removeBtns[i].addEventListener("click", removeFromTotal);
+}
 
 /* const basket = {
   apple: {
