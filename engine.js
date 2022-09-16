a78e3584d31a2d304e3af1a0b6960c12e430014d
Kr33L <105356599+Kr33L@users.noreply.github.com>
2022-09-16 11:48:08 +0100

    locking function added

--- engine.js
+++ engine.js
@@ -1,27 +1,40 @@
 // get elements
 
-const generateBtn = document.querySelector("button");
-const lockBtn = document.querySelector(".lock");
+const generateBtn = document.querySelector(".new-palette-btn");
+const lockBtn = document.querySelectorAll(".lock-btn");
 const color = document.querySelectorAll(".color");
 const hex = document.querySelectorAll(".hex");
 
 generateBtn.addEventListener("click", generateColors);
 
 function generateColors() {
-  for (let i = 0; i < color.length; i++) {
-    //generate random color and set background
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    color[i].style.background = "#" + randomColor;
-    hex[i].style.background = "#" + randomColor;
-    hex[i].innerHTML = randomColor;
+  for (let i = 0; i < lockBtn.length; i++) {
+    lockBtn[i].addEventListener("click", () => {
+      lockBtn[i].classList.add("locked");
+    });
+
+    lockBtn[i].addEventListener("dblclick", () => {
+      lockBtn[i].classList.remove("locked");
+    });
+
+    if (lockBtn[i].classList.contains("locked")) {
+      continue;
+    } else {
+      const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+      color[i].style.backgroundColor = randomColor;
+      hex[i].style.background = randomColor;
+      hex[i].innerHTML = randomColor;
+    }
 
     //animation
     color[i].classList.add("fade-in");
     hex[i].classList.add("fade-in");
-    setTimeout(() => color[i].classList.remove("fade-in"), 500);
-    setTimeout(() => hex[i].classList.remove("fade-in"), 500);
+    setTimeout(() => {
+      color[i].classList.remove("fade-in");
+      hex[i].classList.remove("fade-in");
+    }, 400);
 
-    console.log("log1: colors generated.");
+    console.log("color generated");
   }
 }
 
