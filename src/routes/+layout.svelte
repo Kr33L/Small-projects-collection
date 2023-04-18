b439ba031db065b04fcd93da5dec566961c5b767
Kr33L <105356599+Kr33L@users.noreply.github.com>
2023-04-18 17:43:34 +0100

    converted to svelte and included bastion tribute

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<nav>
+	<a href="/">home</a>
+	<a href="/projects/legacy/bastion">bastion</a>
+</nav>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<div class="img--container">
+	<img class="img" src="$lib/images/construction.gif" alt="Under Construction!" />
+</div>
