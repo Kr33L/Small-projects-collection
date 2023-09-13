29f1873a30953c083900bfa3cc75d5e5987121f5
Kr33L <105356599+Kr33L@users.noreply.github.com>
2023-09-13 18:41:15 +0100

    Implemented CV display and download link on the resume page.

--- src/app/resume/page.js
+++ src/app/resume/page.js
@@ -1,3 +1,17 @@
 export default function Resume() {
-	return <div>This is the resume page, it&apos;s pretty good isn&apos;t it?</div>;
+	return (
+		<div className="flex justify-center">
+			<span> Download my resume&nbsp;</span>
+			<a
+				href="/Karol_Olechno_resume.pdf"
+				className="underline"
+				download="Karol_Olechno_resume">
+				here!
+			</a>
+			<iframe
+				className="container h-screen w-full"
+				src="/Karol_Olechno_resume.pdf"
+			/>
+		</div>
+	);
 }
