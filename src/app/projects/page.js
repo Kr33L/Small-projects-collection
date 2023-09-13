9d4f05251e27be8b3facf774d290e5702bf3edf5
Kr33L <105356599+Kr33L@users.noreply.github.com>
2023-09-13 21:56:59 +0100

    Added "-Page" suffix to function names.

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,3 +1,9 @@
-export default function Projects() {
-	return <div>This is the projects page, check out all these cool projects!</div>;
+export const metadata = {
+	title: 'Karol Olechno ( Projects Page )',
+};
+
+export default function ProjectsPage() {
+	return (
+		<div>This is the projects page, check out all these cool projects!</div>
+	);
 }
