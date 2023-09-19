0d915852c4897653a21ee24dade10f8b630fa42e
Karol <karol@pop-os.localdomain>
2023-09-19 20:51:37 +0100

    Updated config files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+	async redirects() {
+		return [
+			{
+				source: '/',
+				destination: '/resume',
+				permanent: false,
+			},
+			{
+				source: '/blog',
+				destination: '/resume',
+				permanent: false,
+			},
+			{
+				source: '/projects',
+				destination: '/resume',
+				permanent: false,
+			},
+		];
+	},
+	webpack: (config) => {
+		config.externals = [...config.externals, 'canvas', 'jsdom'];
+		return config;
+	},
+};
+
+module.exports = nextConfig;
