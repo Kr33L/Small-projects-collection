1ca7f1d8d49ce99aa740aff3dc8192387c033bc4
ko-karol <karol.olechno1@gmail.com>
2023-09-21 21:20:31 +0100

    Changed text

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,7 +1,3 @@
-export const metadata = {
-	title: 'Karol Olechno ( Projects Page )',
-};
-
 export default function ProjectsPage() {
 	return (
 		<div>This is the projects page, check out all these cool projects!</div>
