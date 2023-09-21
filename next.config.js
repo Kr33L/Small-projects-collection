bdb1680801590e49f497713e17ac79278f7537fa
ko-karol <karol.olechno1@gmail.com>
2023-09-21 18:45:59 +0100

    Removed redirects

--- next.config.js
+++ next.config.js
@@ -1,25 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-	async redirects() {
-		return [
-			{
-				source: '/',
-				destination: '/resume',
-				permanent: false,
-			},
-			{
-				source: '/blog',
-				destination: '/resume',
-				permanent: false,
-			},
-			{
-				source: '/projects',
-				destination: '/resume',
-				permanent: false,
-			},
-		];
-	},
 	webpack: (config) => {
 		config.externals = [...config.externals, 'canvas', 'jsdom'];
 		return config;
