14d1c27b930df07d39ff32c780d9bfbfa29887c3
ko-karol <karol.olechno1@gmail.com>
2023-10-01 01:49:43 +0100

    added tailwind support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
 	darkMode: 'class',
 	content: [
@@ -7,20 +10,34 @@ module.exports = {
 		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
 	],
 	theme: {
-		colors: {
-			text: '#020303',
-			bg: '#f5f7fa',
-			primary: '#9a4288',
-			secondary: '#f0e9db',
-			accent: '#4db35f',
-			dark: {
-				text: '#fcfdfd',
-				bg: '#05070a',
-				primary: '#bd65ac',
-				secondary: '#241d0f',
-				accent: '#4db35f',
+		extend: {
+			textShadow: {
+				DEFAULT:
+					'0 2px 4px var(--tw-shadow-color), 2px 7px 5px rgba(0,0,0,0.3), 0px -4px 10px rgba(255,255,255,0.3)',
+			},
+			boxShadow: {
+				custom:
+					'inset 0 0 0.5px 1px hsla(0, 0%, 100%, 0.075), 0 0 0 1px hsla(0, 0%, 0%, 0.05), 0 0.3px 0.4px hsla(0, 0%, 0%, 0.02), 0 0.9px 1.5px hsla(0, 0%, 0%, 0.045), 0 3.5px 6px hsla(0, 0%, 0%, 0.09)',
 			},
 		},
+		colors: {
+			text: 'var(--text)',
+			background: 'var(--background)',
+			primary: 'var(--primary)',
+			secondary: 'var(--secondary)',
+			accent: 'var(--accent)',
+		},
 	},
-	plugins: [],
+	plugins: [
+		plugin(function ({ matchUtilities, theme }) {
+			matchUtilities(
+				{
+					'text-shadow': (value) => ({
+						textShadow: value,
+					}),
+				},
+				{ values: theme('textShadow') },
+			);
+		}),
+	],
 };
