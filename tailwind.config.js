92bb4e211c7a05e0b36b35abdb603f6bc9692027
ko-karol <karol.olechno1@gmail.com>
2023-10-01 15:13:14 +0100

    Purged unused styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,16 +10,7 @@ module.exports = {
 		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
 	],
 	theme: {
-		extend: {
-			textShadow: {
-				DEFAULT:
-					'0 2px 4px var(--tw-shadow-color), 2px 7px 5px rgba(0,0,0,0.3), 0px -4px 10px rgba(255,255,255,0.3)',
-			},
-			boxShadow: {
-				custom:
-					'inset 0 0 0.5px 1px hsla(0, 0%, 100%, 0.075), 0 0 0 1px hsla(0, 0%, 0%, 0.05), 0 0.3px 0.4px hsla(0, 0%, 0%, 0.02), 0 0.9px 1.5px hsla(0, 0%, 0%, 0.045), 0 3.5px 6px hsla(0, 0%, 0%, 0.09)',
-			},
-		},
+		extend: {},
 		colors: {
 			text: 'var(--text)',
 			background: 'var(--background)',
@@ -28,16 +19,5 @@ module.exports = {
 			accent: 'var(--accent)',
 		},
 	},
-	plugins: [
-		plugin(function ({ matchUtilities, theme }) {
-			matchUtilities(
-				{
-					'text-shadow': (value) => ({
-						textShadow: value,
-					}),
-				},
-				{ values: theme('textShadow') },
-			);
-		}),
-	],
+	plugins: [],
 };
