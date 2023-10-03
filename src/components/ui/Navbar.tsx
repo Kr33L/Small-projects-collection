9e405bf2fe3dec31f0f0805753cc085466fb673b
ko-karol <karol.olechno1@gmail.com>
2023-10-03 23:32:01 +0100

    feat: finally got a navbar I'm happy with

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,20 +1,34 @@
 'use client';
 
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+import ThemeToggle from './ThemeToggle';
+import Wrapper from './Wrapper';
 
 export default function Navbar() {
+	const pathname = usePathname();
+
 	return (
-		<nav className="flex justify-evenly py-2">
-			{[
-				['Home', '/'],
-				['Projects', '/projects'],
-				['Blog', '/blog'],
-				['Resume', '/resume'],
-			].map(([title, url]) => (
-				<div key={title} className="px-4">
-					<Link href={url}>{title}</Link>
-				</div>
-			))}
+		<nav className="fixed bottom-0 left-1/2 flex w-11/12 -translate-x-1/2 transform items-center justify-between sm:relative sm:w-4/5">
+			<Wrapper className="flex w-full justify-around sm:space-x-2">
+				{[
+					['Home', '/'],
+					['Projects', '/projects'],
+					['Blog', '/blog'],
+					['Resume', '/resume'],
+				].map(([title, url]) => (
+					<Link
+						href={url}
+						key={title}
+						className={clsx(
+							pathname == url && 'bg-secondary drop-shadow-sm',
+							'w-full border-b border-accent py-1 text-center transition-all duration-100 hover:shadow-lg active:shadow-inner',
+						)}>
+						<span>{title}</span>
+					</Link>
+				))}
+			</Wrapper>
 		</nav>
 	);
 }
