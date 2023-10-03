dcf803e0fc88261b4195319debb7d3e0fe43fbc7
ko-karol <karol.olechno1@gmail.com>
2023-10-03 23:35:29 +0100

    refactor: changed layout, much more simplified and easier to work with

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 import { M_PLUS_1_Code } from 'next/font/google';
 import { clsx } from 'clsx';
 
-import ThemeProvider from '@/components/ThemeProvider';
+import ThemeProvider from '@/lib/ThemeProvider';
 import Wrapper from '@/components/ui/Wrapper';
 import Navbar from '@/components/ui/Navbar';
 import ThemeToggle from '@/components/ui/ThemeToggle';
@@ -10,28 +10,24 @@ import ThemeToggle from '@/components/ui/ThemeToggle';
 const font = M_PLUS_1_Code({ subsets: ['latin'] });
 
 export const metadata = {
-	title: 'Karol Olechno',
+	title: 'Karol Olechno ',
 	description:
 		"Karol Olechno's personal website showcasing his projects, skills and more.",
 };
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang="en" suppressHydrationWarning>
-			<body>
+		<html lang="en" suppressHydrationWarning className="h-screen">
+			<body
+				className={clsx(
+					font.className,
+					'h-screen border border-accent bg-background text-xl text-text transition-all duration-100',
+				)}>
 				<ThemeProvider attribute="class">
-					<Wrapper
-						className={clsx(
-							font.className,
-							'mx-auto max-w-screen-lg border bg-background text-text',
-						)}>
-						<Wrapper className="flex max-w-prose justify-between">
-							<ThemeToggle />
-						</Wrapper>
-						<Wrapper className="w-full border bg-background">
-							<Navbar />
-						</Wrapper>
-						<Wrapper>{children}</Wrapper>
+					<Wrapper className="mx-auto my-auto h-full max-w-screen-lg border-x">
+						<ThemeToggle className="fixed top-0 z-50 flex flex-col sm:-translate-x-12" />
+						<Navbar />
+						{children}
 					</Wrapper>
 				</ThemeProvider>
 			</body>
