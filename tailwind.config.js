d6a12235573056cfeba188cdf2f3355a970757a1
ko-karol <karol.olechno1@gmail.com>
2023-10-03 23:28:18 +0100

    feat: changed color scheme and added some animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
-const plugin = require('tailwindcss/plugin');
-
 module.exports = {
+	mode: 'jit',
 	darkMode: 'class',
 	content: [
 		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -10,13 +9,35 @@ module.exports = {
 		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
 	],
 	theme: {
-		extend: {},
-		colors: {
-			text: 'var(--text)',
-			background: 'var(--background)',
-			primary: 'var(--primary)',
-			secondary: 'var(--secondary)',
-			accent: 'var(--accent)',
+		extend: {
+			colors: {
+				text: 'var(--text)',
+				background: 'var(--background)',
+				primary: 'var(--primary)',
+				secondary: 'var(--secondary)',
+				accent: 'var(--accent)',
+			},
+
+			keyframes: {
+				horizontal: {
+					'0%': { transform: 'translateX(0)' },
+					'100%': { transform: 'translateX(20px)' },
+				},
+				vertical: {
+					'0%': { transform: 'translateY(0)' },
+					'100%': { transform: 'translateY(20px)' },
+				},
+
+				expand: {
+					'0%': { transform: 'scale(0)' },
+					'100%': { transform: 'scale(1)' },
+				},
+			},
+			animation: {
+				slideH: 'horizontal 1s ease-in-out',
+				slideV: 'vertical 1s ease-in-out',
+				expand: 'expand 0.5s ease-in-out',
+			},
 		},
 	},
 	plugins: [],
