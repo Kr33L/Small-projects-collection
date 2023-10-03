909cda99ee3b3dad2882a9e666345f40125fad6f
ko-karol <karol.olechno1@gmail.com>
2023-10-03 23:37:07 +0100

    feat: added metadata + removed P tag

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,9 @@
-import Paragraph from '@/components/ui/Paragraph';
-
-const metaData = {
-	title: 'Blog',
-	description: 'Karol Olechno | This is my blog page!',
+export const metadata = {
+	title: 'Karol Olechno | Blog',
+	description:
+		"Karol Olechno's blog page, here you can find all of his blog posts.",
 };
 
 export default function BlogPage() {
-	return <Paragraph>This is ||theblogs||!</Paragraph>;
+	return <p>This is my blog page!</p>;
 }
