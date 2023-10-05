895d4fcbea66a800074c97ffa2ced242c931bb8d
ko-karol <karol.olechno1@gmail.com>
2023-10-05 09:56:43 +0100

    feat: scales according to container width (hardcoded at min-width:1024

--- src/hooks/useWindowScale.ts
+++ src/hooks/useWindowScale.ts
@@ -3,18 +3,13 @@
 import { useState, useEffect } from 'react';
 
 export default function useWindowScale() {
-	const [scale, setScale] = useState(2);
+	const [scale, setScale] = useState(1.6);
 
 	useEffect(() => {
-		const divideWidth = (width: number): number => {
-			return Number((document.body.clientWidth / width).toFixed(2));
-		};
 		const handleResize = () => {
-			setScale(() =>
-				document.body.clientWidth >= 1024
-					? divideWidth(1500)
-					: divideWidth(700),
-			);
+			const width = window.innerWidth;
+			const ratio = (width <= 1024 && (width / 1024) * 1.6) || 1.6;
+			setScale(ratio);
 		};
 
 		if (typeof window !== 'undefined') {
