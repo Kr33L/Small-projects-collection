38efb70a9c50fc1ffb51e2021d852ce5426513e6
ko-karol <karol.olechno1@gmail.com>
2023-10-12 20:11:14 +0100

    refactored: added theme mapping

--- src/components/ui/ThemeToggle.tsx
+++ src/components/ui/ThemeToggle.tsx
@@ -1,35 +1,35 @@
 'use client';
 
 import { useTheme } from 'next-themes';
-import { useEffect } from 'react';
 import { MoonIcon, DesktopIcon, SunIcon } from '@radix-ui/react-icons';
 import useMounted from '@/hooks/useMounted';
-import Wrapper from './Wrapper';
 import clsx from 'clsx';
 
 export default function ThemeToggle({ className }: { className?: string }) {
 	const mounted = useMounted();
 	const { theme, setTheme } = useTheme();
 
-	useEffect(() => {
-		setTheme(theme);
-	}, [setTheme, theme]);
+	const themeMap = [
+		{ mode: 'light', title: 'Light theme', icon: <SunIcon /> },
+		{ mode: 'system', title: 'System theme', icon: <DesktopIcon /> },
+		{ mode: 'dark', title: 'Dark theme', icon: <MoonIcon /> },
+	];
 
 	if (!mounted) return null; // SSR
 	return (
-		<Wrapper className={className}>
-			{['light', 'system', 'dark'].map((mode) => (
+		<div className={className}>
+			{themeMap.map(({ mode, title, icon }) => (
 				<button
 					key={mode}
-					title={{ light: 'Light theme', system: 'System theme', dark: 'Dark theme' }[mode]}
+					title={title}
 					onClick={() => setTheme(mode)}
 					className={clsx(
-						theme == mode ? 'bg-secondary drop-shadow-2xl' : 'bg-background',
-						'border-b border-accent p-1 duration-0 after:absolute after:bottom-0 after:left-0 after:right-0 after:bg-accent hover:shadow-lg hover:after:h-px active:shadow-inner sm:relative',
+						theme === mode ? 'bg-secondary drop-shadow-2xl' : 'bg-background',
+						'border-b border-accent p-1 hover:border hover:shadow-lg hover:after:h-px active:shadow-inner sm:relative',
 					)}>
-					{{ light: <SunIcon />, system: <DesktopIcon />, dark: <MoonIcon /> }[mode]}
+					{icon}
 				</button>
 			))}
-		</Wrapper>
+		</div>
 	);
 }
